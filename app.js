d47c8957389bb0412b3fe255a9ca12da5cd0b4c6
Owais Badat <owaisbadat@gmail.com>
2018-11-01 00:27:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+var bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express')
+
+let movieCollection = []
+
+app.use(express.static('css'))
+
+app.use(bodyParser.urlencoded({ extended : false}))
+
+app.engine('mustache', mustacheExpress())
+
+app.set('views', './views')
+
+app.set('view engine', 'mustache')
+
+app.get('/',function(req,res){
+    res.render('index')
+
+})
+
+
+app.post('/delete-movie',function(req,res){
+
+    let movieTitle = req.body.movieTitle
+    // console.log(cityName)
+
+    movieCollection = movieCollection.filter(function(movie){
+      return movie.movieTitle != movieTitle
+    })
+
+    res.redirect('/my-movie-collection')
+})
+
+app.get('/add-movie',function(req,res){
+  res.render('./add-movie')
+})
+
+
+app.post('/add-movie',function(req,res){
+
+  let movieTitle = req.body.movieTitle
+  let movieGenre = req.body.movieGenre
+  let movieDescription = req.body.movieDescription
+  let moviePoster = req.body.moviePosterURL
+
+  movieCollection.push({ movieTitle : movieTitle, movieGenre : movieGenre, movieDescription : movieDescription, moviePosterURL : moviePoster })
+
+  res.redirect('/my-movie-collection')
+
+})
+
+app.get('/my-movie-collection',(req,res) => {
+
+  res.render('my-movie-collection',{ collectionList : movieCollection })
+})
+
+// app.get('/my-movie-collection/:genre', function(req, res){
+//    let genre = req.params.movieGenre
+//    console.log(genre)
+//    res.render("my-movie-collection", {collectionList: movieCollection.filter(function(movie){
+//        return movie.genre == genre
+//    }), filters: genre})
+// })
+
+
+
+
+app.listen(PORT, function(){
+  console.log("Server is running...")
+})
